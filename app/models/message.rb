b69108318358fe09c3f2b5fd26ff01b7952f1196
tomball357 <tball@chicagobooth.edu>
2025-03-22 17:07:52 +0000

    check

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,10 +7,10 @@
 #  role       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  quiz_id    :integer
+#  sim_id    :integer
 #
 class Message < ApplicationRecord
-  belongs_to :quiz
+  belongs_to :sim
 
   validates :role, inclusion: { in: [ "system", "user", "assistant" ] }
   validates :role, presence: true
--- app/models/sim.rb
+++ app/models/sim.rb
@@ -1,13 +1,13 @@
 # == Schema Information
 #
-# Table name: quizzes
+# Table name: vbooks
 #
 #  id         :bigint           not null, primary key
 #  topic      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-class Quiz < ApplicationRecord
+class Sim < ApplicationRecord
   has_many  :messages, dependent: :destroy
 
   validates :topic, presence: true
